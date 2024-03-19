65761fe5a1975eeb581dd507da6720aac0a28af9
yashreddy28 <152617699+yashreddy28@users.noreply.github.com>
2024-03-19 13:00:34 +0000

    Add files via upload

---
+++ subprogram 1.py
@@ -0,0 +1,8 @@
+number1=int(input("Enter a number:"))
+number2=int(input("Enter another number :"))
+
+def multiply(number1,number2):
+    total = number1*number2
+    print(total)
+
+multiply(number1,number2)
