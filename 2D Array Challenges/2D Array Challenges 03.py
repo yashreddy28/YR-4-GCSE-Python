b7d9b430222a5c70dbb26a5818ca070264d129d4
yashreddy28 <152617699+yashreddy28@users.noreply.github.com>
2024-04-30 13:02:20 +0100

    Add files via upload

---
+++ 2D Array Challenges/2D Array Challenges 03.py
@@ -0,0 +1,12 @@
+Stuff = [
+        ["1" , "2" , "3" , "4"],
+        ["a" , "b" , "c" , "d"],
+        ["I" , "II" , "III" , "IV"],
+        ]
+
+print(Stuff)
+
+Stuff[1][2] = "Croydon"
+
+print(Stuff)
+
