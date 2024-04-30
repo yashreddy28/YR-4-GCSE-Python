a1728f36c6f896fef27d4640610e829a92c77b82
yashreddy28 <152617699+yashreddy28@users.noreply.github.com>
2024-04-30 12:37:43 +0100

    Add files via upload

---
+++ 2D Array Challenges/2D Array Challenges 02.py
@@ -0,0 +1,11 @@
+MySpaceCompany = ["Rocket Pack" , "Moon Base" , "Moon Buggy"]
+
+print(MySpaceCompany)
+
+MySpaceCompany[0] = "Rich people going to Mars"
+
+print(MySpaceCompany)
+
+MySpaceCompany = ["Rich people going to the Moon" , "Moon Base" , "Moon Buggy"]
+
+print(MySpaceCompany)
